bc9f748a4cee6d42c236f5e0865d1bebfe650883
Denise Onukwue <donukwue1@gmail.com>
2021-04-14 14:26:44 -0400

    delete basics controller and route

--- routes/basics.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const basicsCtrl = require('../controllers/basics');
-
-router.post('/checklists/:id/basics', basicsCtrl.create);
-
-module.exports= router
